a302a0687e80d15f680079e43187511e16d23079
Jeremy Voros <jeremyvoros@gmail.com>
2024-12-11 10:52:20 -0700

    shadcn setup

--- client/components/Home.vue
+++ client/components/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ofetch } from "ofetch";
 import { store } from "../store.js";
+import { Button } from "@/components/ui/button/index.js";
 
 const logout = async () => {
   const res = await ofetch("/api/logout", {
@@ -16,7 +17,8 @@ const logout = async () => {
 </script>
 <template>
   <div>
-    <h1>Home</h1>
+    <h1 class="mb-8 bg-slate-600">Home</h1>
     <button @click="logout">Logout</button>
+    <Button>Shadcn</Button>
   </div>
 </template>
