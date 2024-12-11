ae1fec0a7a106ea8fbd8e1a97f1d635f2faf481c
Jeremy Voros <jeremyvoros@gmail.com>
2024-12-11 10:52:20 -0700

    shadcn setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import { fileURLToPath, URL } from "node:url";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
@@ -17,12 +20,22 @@ export default defineConfig({
     outDir: "../dist/client",
     emptyOutDir: true,
   },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   server: {
     proxy: {
       // with options
       "/api": {
         target: "http://localhost:3000",
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("/shadcn-vue", import.meta.url)),
     },
   },
 });
