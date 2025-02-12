b99e99a57d786eb71d0698ac80ac587ea2e452d1
Jeremy Voros <jeremyvoros@gmail.com>
2025-02-12 12:05:27 -0700

    clarity around board

--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
 ## TODO
 
+- [ ] Sort out package.json scripts, .env vars, deploy scripts
+- [ ] Deploy dev and prod database
 - [ ] Server doesn't ever re-poll db for changes, so if db changes independent of server, frontend never updates
 - [ ] Handle case where optimistic update fails
--- server/board.js
+++ server/board.js
@@ -3,10 +3,10 @@ import { jwt } from "hono/jwt";
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-import Board from "./core/board.js";
+import BoardCore from "./core/board.js";
 import { broadcast } from "./stream.js";
 
-const board = new Hono();
+const boardRoutes = new Hono();
 const prisma = new PrismaClient().$extends(withAccelerate());
 
 // HELPERS
@@ -28,7 +28,7 @@ const getSiteDetails = async (site) => {
 
 const broadcastBoard = async (site) => {
   console.log(`[${site}] board broadcasted`);
-  broadcast(sites[site].clients, "board", Board.hydrate(sites[site].board));
+  broadcast(sites[site].clients, "board", BoardCore.hydrate(sites[site].board));
 };
 
 // SETUP SITES
@@ -41,24 +41,24 @@ export const sites = {
 };
 
 // MIDDLEWARE
-board.use("/*", jwt({ secret: process.env.JWT_SECRET, cookie: "auth" }));
+boardRoutes.use("/*", jwt({ secret: process.env.JWT_SECRET, cookie: "auth" }));
 
 // ROUTES
 
-board.get("/getBoard", async (c) => {
+boardRoutes.get("/getBoard", async (c) => {
   const site = c.get("jwtPayload").site;
   return c.json({ data: sites[site].board });
 });
 
-board.get("/getSiteDetails", async (c) => {
+boardRoutes.get("/getSiteDetails", async (c) => {
   const site = c.get("jwtPayload").site;
   return c.json({ data: sites[site].details });
 });
 
-board.post("/boardReset", async (c) => {
+boardRoutes.post("/boardReset", async (c) => {
   const site = c.get("jwtPayload").site;
   const board = sites[site].board;
-  const logs = Board.buildLogs(site, board);
+  const logs = BoardCore.buildLogs(site, board);
   const date = board.date;
 
   prisma.$transaction([
@@ -66,7 +66,7 @@ board.post("/boardReset", async (c) => {
     prisma.log.createMany({ data: logs }),
   ]);
 
-  const newBoard = Board.reset(board);
+  const newBoard = BoardCore.reset(board);
 
   try {
     await prisma.site.update({
@@ -81,9 +81,10 @@ board.post("/boardReset", async (c) => {
     return c.json({ error: error.message });
   }
 });
+
 // PROGRAMMATIC ROUTES
 
-// all the functions are just passed through to core Board
+// all the functions are just passed through to core BoardCore
 // same JWT and payload extraction
 // same error handling
 // same broadcast
@@ -91,10 +92,14 @@ board.post("/boardReset", async (c) => {
 const wrapWithUpdate = (fn) => async (c) => {
   const site = c.get("jwtPayload").site;
   const payload = await c.req.json();
-  const newBoard = Board[fn](sites[site].board, ...Object.keys(payload).map((key) => payload[key]));
+  const newBoard = BoardCore[fn](
+    sites[site].board,
+    ...Object.keys(payload).map((key) => payload[key])
+  );
+  // optimistically update board and broadcast
   sites[site].board = newBoard;
   broadcastBoard(site);
-
+  // then update db, assuming it will work
   try {
     await prisma.site.update({
       where: { site },
@@ -107,7 +112,7 @@ const wrapWithUpdate = (fn) => async (c) => {
   }
 };
 
-// spit out endpoint for each fn of Board
-Object.keys(Board).forEach((fn) => board.post(`/${fn}`, wrapWithUpdate(fn)));
+// generate endpoint for each fn of Board
+Object.keys(BoardCore).forEach((fn) => boardRoutes.post(`/${fn}`, wrapWithUpdate(fn)));
 
-export default board;
+export default boardRoutes;
