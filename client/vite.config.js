346b9cdb0c807c3602378734cbf5ad190d83a802
Jeremy Voros <jeremyvoros@gmail.com>
2025-05-25 11:00:33 -0600

    fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
     proxy: {
       // with options
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:8080",
       },
     },
   },
