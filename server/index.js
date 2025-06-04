88303ef6948915e7182d416b029f27acd4c0760c
Jeremy Voros <jeremyvoros@gmail.com>
2025-06-04 09:47:16 -0600

    websocket basic install

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import { Server } from "socket.io";
 import apiRoutes from "./routes/apiRoutes.js";
 
+// START SERVER
 const app = new Hono();
+const port = 8080;
+const httpServer = serve({
+  fetch: app.fetch,
+  port,
+});
+console.log(`Server is running on http://localhost:${port}`);
+
+// ADD WEBSOCKETS
+export const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("[socket] id:", socket.id);
+  io.emit("connection", "connected with ID: " + socket.id);
+});
+
+// ROUTES
 
 app.route("/api", apiRoutes);
 
-// VITE BUILT FILES
+// VITE routes
 
 // paths relative to project root, not this file
 // client/assets
@@ -16,12 +34,3 @@ app.use("/assets/*", serveStatic({ root: "./dist/client" }));
 app.use("/*", serveStatic({ root: "./dist/client/" }));
 // client/index.html
 app.use("/", serveStatic({ path: "./dist/client/index.html" }));
-
-// START
-const port = 8080;
-console.log(`Server is running on http://localhost:${port}`);
-
-serve({
-  fetch: app.fetch,
-  port,
-});
